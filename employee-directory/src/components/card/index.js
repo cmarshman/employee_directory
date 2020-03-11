09993ecbbb35e1e94a2ccd33ced2f72d5906a791
Cory Marshman <marshman.cory@gmail.com>
2020-03-10 21:21:59 -0400

    JSON and photo issues fixed

--- employee-directory/src/components/card/index.js
+++ employee-directory/src/components/card/index.js
@@ -8,20 +8,22 @@ function Card() {
         <>
             <br />
             <br />
-            
-            <div >
+            <div className="columns">
+                <div className="column is-full"><br/>
                 {employees.map(item => (
-                    <article key={item.id} className="tile is-child notification is-light">
-                        <p className="title" id="name">{item.name}</p>
-                        <p className="subtitle" id="position">{item.position}</p>
-                        <p cid="start_date">{item.start_date}</p>
-                        <img  id="head_shot" src={item.photo} alt={item.name} />
-                        <p className="subtitle "><strong>Contact Information</strong></p>
-                        <p  id="phone">{item.contact_email}</p>
-                        <p  id="email">{item.contact_number}</p>
-                        <p  id="department">{item.department}</p>
+                    <article key={item.id} className="tile notification has-text-centered" id="block">
+                            <p className="title" id="name">{item.name}</p>
+                            <p className="subtitle" id="position">{item.position}</p>
+                            <p cid="start_date">{item.start_date}</p>
+                            <img id="head_shot" src={item.photo} alt={item.name} />
+                            <p className="subtitle "><strong>Contact Information</strong></p>
+                            <p id="phone">{item.contact_email}</p>
+                            <p id="email">{item.contact_number}</p>
+                            <p id="department">{item.department}</p>
+                            <hr />
                     </article>
-            ))}
+                ))}
+                </div>
             </div>
         </>
     );
--- employee-directory/src/components/input/index.js
+++ employee-directory/src/components/input/index.js
@@ -5,15 +5,15 @@ import './style.css';
 
 function Input(){
     return (
-        <div class="tile is-ancestor">
-        <div class="tile is vertical is-11" id="tile">
-          <div class="tile is-parent">
-            <article class="tile is-child notification" id="block">
-            <div class="field" id="search_employee">
-                <p class="control has-icons-left">
-                <input class="input" type="text" placeholder="Search by Emplolyee Name"/>
-                <span class="icon is-small is-left">
-                    <i class="fas fa-user-circle"></i>
+        <div className="tile is-ancestor">
+        <div className="tile is vertical is-11" id="tile">
+          <div className="tile is-parent">
+            <article className="tile is-child notification" id="block">
+            <div className="field" id="search_employee">
+                <p className="control has-icons-left">
+                <input className="input" type="text" placeholder="Search by Emplolyee Name"/>
+                <span className="icon is-small is-left">
+                    <i className="fas fa-user-circle"></i>
                 </span> 
             </p>
             </div>
